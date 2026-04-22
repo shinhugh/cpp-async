390a74b5b26f60e26671ea6929a84efb2a7d35fe
Hugh Shin <hughbee135@gmail.com>
2026-04-22 01:34:05 -0700

    Add calls on Future Require* functions to test program

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -2,6 +2,7 @@
 #include "async/future.h"
 
 #include <functional>
+#include <vector>
 
 // -----------------------------------------------------------------------------
 
@@ -18,6 +19,9 @@ int main()
 
 int Program()
 {
+  std::vector<async::Future<void>> voidFutures;
+  std::vector<async::Future<int>> intFutures;
+
   async::RunTaskOnNewThread<void>([]()
     {
       async::RunTaskOnNewThread<void>([]() {}).Await();
@@ -43,6 +47,26 @@ int Program()
       async::RunTaskOnNewCoroutine<int>([]() { return 2; }).Await();
       return 3;
     }).Await();
+
+  voidFutures.push_back(async::RunTaskOnNewThread<void>([]() {}));
+  voidFutures.push_back(async::RunTaskOnNewCoroutine<void>([]() {}));
+  async::Future<void>::RequireAll(voidFutures).Await();
+  voidFutures.clear();
+
+  intFutures.push_back(async::RunTaskOnNewThread<int>([]() { return 1; }));
+  intFutures.push_back(async::RunTaskOnNewCoroutine<int>([]() { return 2; }));
+  async::Future<int>::RequireAll(intFutures).Await();
+  intFutures.clear();
+
+  voidFutures.push_back(async::RunTaskOnNewThread<void>([]() {}));
+  voidFutures.push_back(async::RunTaskOnNewCoroutine<void>([]() {}));
+  async::Future<void>::RequireOne(voidFutures).Await();
+  voidFutures.clear();
+
+  intFutures.push_back(async::RunTaskOnNewThread<int>([]() { return 1; }));
+  intFutures.push_back(async::RunTaskOnNewCoroutine<int>([]() { return 2; }));
+  async::Future<int>::RequireOne(intFutures).Await();
+  intFutures.clear();
 
   return 0;
 }
