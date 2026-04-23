6766bc8eb17ed28791e1df64e1f461e4f259fcc4
Hugh Shin <hughbee135@gmail.com>
2026-04-23 00:42:58 -0700

    Define class Promise

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ mkdir -p build
 mkdir -p build/async
 clang++ src/async/async.cpp -c -o build/async/async.o -Isrc -D PLATFORM_POSIX -g -Wall -Wextra
 clang++ src/async/future.cpp -c -o build/async/future.o -Isrc -D PLATFORM_POSIX -g -Wall -Wextra
+clang++ src/async/promise.cpp -c -o build/async/promise.o -Isrc -D PLATFORM_POSIX -g -Wall -Wextra
 
 mkdir -p build/main
 clang++ src/main/main.cpp -c -o build/main/main.o -Isrc -D PLATFORM_POSIX -g -Wall -Wextra
