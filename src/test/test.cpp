58470d96a203e7352eb8027494b1872269cda648
Hugh Shin <hughbee135@gmail.com>
2026-04-23 00:31:46 -0700

    Add calls on RunTaskOnNew* functions and Future::Await() to test program

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,4 +1,5 @@
 #include "async/async.h"
+#include "async/future.h"
 
 #include <functional>
 
@@ -17,5 +18,31 @@ int main()
 
 int Program()
 {
+  async::RunTaskOnNewThread<void>([]()
+    {
+      async::RunTaskOnNewThread<void>([]() {}).Await();
+      async::RunTaskOnNewCoroutine<void>([]() {}).Await();
+    }).Await();
+
+  async::RunTaskOnNewThread<int>([]()
+    {
+      async::RunTaskOnNewThread<int>([]() { return 1; }).Await();
+      async::RunTaskOnNewCoroutine<int>([]() { return 2; }).Await();
+      return 3;
+    }).Await();
+
+  async::RunTaskOnNewCoroutine<void>([]()
+    {
+      async::RunTaskOnNewThread<void>([]() {}).Await();
+      async::RunTaskOnNewCoroutine<void>([]() {}).Await();
+    }).Await();
+
+  async::RunTaskOnNewCoroutine<int>([]()
+    {
+      async::RunTaskOnNewThread<int>([]() { return 1; }).Await();
+      async::RunTaskOnNewCoroutine<int>([]() { return 2; }).Await();
+      return 3;
+    }).Await();
+
   return 0;
 }
