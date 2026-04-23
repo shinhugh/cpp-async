9997ad2f14d3550b167a91d09a623c605a922b73
Hugh Shin <hughbee135@gmail.com>
2026-04-23 00:30:46 -0700

    Define class Future

--- iwyu.sh
+++ iwyu.sh
@@ -3,6 +3,7 @@
 # Must be run from project's root directory
 
 include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/async/async.cpp
+include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/async/future.cpp
 
 include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/main/main.cpp
 
