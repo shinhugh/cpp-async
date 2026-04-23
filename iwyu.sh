da936994a819b2d5467bef39046c4bc14172f4b7
Hugh Shin <hughbee135@gmail.com>
2026-04-23 00:42:58 -0700

    Define class Promise

--- iwyu.sh
+++ iwyu.sh
@@ -4,6 +4,7 @@
 
 include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/async/async.cpp
 include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/async/future.cpp
+include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/async/promise.cpp
 
 include-what-you-use -Xiwyu --no_fwd_decls -Xiwyu --quoted_includes_first -Isrc -D PLATFORM_POSIX src/main/main.cpp
 
