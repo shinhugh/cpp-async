fc2c1cddd9ce37fd773553142b5aba9001d72f2c
Hugh Shin <hughbee135@gmail.com>
2026-04-23 03:35:28 -0700

    Implement *ThreadLocalThreadTaskContext functions

--- src/async/thread_local_task_context.cpp
+++ src/async/thread_local_task_context.cpp
@@ -1,12 +1,30 @@
 #include "thread_local_task_context.h"
 
+#include <mutex>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+
+// -----------------------------------------------------------------------------
+
+static std::unordered_map<
+  std::thread::id,
+  async::impl::ThreadLocalThreadTaskContext>
+  s_threadLocalThreadTaskContexts;
+static std::mutex s_threadLocalThreadTaskContextsMutex;
+
 // -----------------------------------------------------------------------------
 
 async::impl::ThreadLocalThreadTaskContext*
   async::impl::GetThreadLocalThreadTaskContext()
 {
-  // TODO: Implement
-  return nullptr;
+  std::lock_guard lock{ s_threadLocalThreadTaskContextsMutex };
+  auto it = s_threadLocalThreadTaskContexts.find(std::this_thread::get_id());
+  if (it == s_threadLocalThreadTaskContexts.end())
+  {
+    return nullptr;
+  }
+  return &it->second;
 }
 
 // -----------------------------------------------------------------------------
@@ -14,13 +32,14 @@ async::impl::ThreadLocalThreadTaskContext*
 async::impl::ThreadLocalThreadTaskContext*
   async::impl::CreateThreadLocalThreadTaskContext()
 {
-  // TODO: Implement
-  return nullptr;
+  std::lock_guard lock{ s_threadLocalThreadTaskContextsMutex };
+  return &s_threadLocalThreadTaskContexts[std::this_thread::get_id()];
 }
 
 // -----------------------------------------------------------------------------
 
 void async::impl::DestroyThreadLocalThreadTaskContext()
 {
-  // TODO: Implement
+  std::lock_guard lock{ s_threadLocalThreadTaskContextsMutex };
+  s_threadLocalThreadTaskContexts.erase(std::this_thread::get_id());
 }
