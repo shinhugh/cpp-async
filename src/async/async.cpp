638bac46eef51ff9f26b1859f1aa16541293818a
Hugh Shin <hughbee135@gmail.com>
2026-04-23 14:29:26 -0700

    Implement RunTaskOnNewThread()

--- src/async/async.cpp
+++ src/async/async.cpp
@@ -2,12 +2,29 @@
 
 #include "future.h"
 #include "promise_future_state.h"
+#include "promise.h"
+#include "thread_local_task_context.h"
 
+#include "telemetry/living_span.h"
 #include "telemetry/span.h"
 
+#include <condition_variable>
 #include <functional>
 #include <memory>
+#include <mutex>
 #include <optional>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+// -----------------------------------------------------------------------------
+
+std::unordered_map<std::thread::id, std::thread> async::impl::g_activeThreads;
+std::mutex async::impl::g_activeThreadsMutex;
+std::vector<std::thread> async::impl::g_completeThreads;
+std::mutex async::impl::g_completeThreadsMutex;
+std::condition_variable_any async::impl::g_allTasksCv;
 
 // -----------------------------------------------------------------------------
 
@@ -20,12 +37,70 @@ std::optional<telemetry::Span> async::GetActiveSpan()
 // -----------------------------------------------------------------------------
 
 template <>
-async::Future<void> async::RunTaskOnNewThread(std::function<void()>&& /*task*/)
+async::Future<void> async::RunTaskOnNewThread(std::function<void()>&& task)
 {
   std::shared_ptr<impl::PromiseFutureState<void>> promiseFutureState
     = std::make_shared<impl::PromiseFutureState<void>>();
 
-  // TODO: Implement
+  std::shared_ptr<bool> threadAddedToActivePool = std::make_shared<bool>(false);
+  std::shared_ptr<std::mutex> threadAddedToActivePoolMutex
+    = std::make_shared<std::mutex>();
+  std::shared_ptr<std::condition_variable> threadAddedToActivePoolCv
+    = std::make_shared<std::condition_variable>();
+
+  std::thread childThread{ [
+    task = std::move(task),
+    promise = impl::Promise{ promiseFutureState },
+    span = GetActiveSpan(),
+    threadAddedToActivePool,
+    threadAddedToActivePoolMutex,
+    threadAddedToActivePoolCv]() mutable
+    {
+      impl::ThreadLocalThreadTaskContext* context
+        = impl::CreateThreadLocalThreadTaskContext();
+      context->m_span = std::make_unique<telemetry::LivingSpan>(
+        span ?
+        telemetry::LivingSpan::Create(*span) :
+        telemetry::LivingSpan::Create());
+
+      task();
+
+      context->m_span.reset();
+      impl::DestroyThreadLocalThreadTaskContext();
+
+      promise.Fulfill();
+
+      {
+        std::unique_lock lock{ *threadAddedToActivePoolMutex };
+        while (!*threadAddedToActivePool)
+        {
+          threadAddedToActivePoolCv->wait(lock);
+        }
+      }
+
+      {
+        std::scoped_lock lock{
+          impl::g_activeThreadsMutex,
+          impl::g_completeThreadsMutex };
+        std::thread completeThread
+          = std::move(impl::g_activeThreads.at(std::this_thread::get_id()));
+        impl::g_activeThreads.erase(std::this_thread::get_id());
+        impl::g_completeThreads.push_back(std::move(completeThread));
+      }
+      impl::g_allTasksCv.notify_one();
+    } };
+
+  {
+    std::lock_guard lock{ impl::g_activeThreadsMutex };
+    impl::g_activeThreads.emplace(childThread.get_id(), std::move(childThread));
+  }
+  impl::g_allTasksCv.notify_one();
+
+  {
+    std::lock_guard lock{ *threadAddedToActivePoolMutex };
+    *threadAddedToActivePool = true;
+  }
+  threadAddedToActivePoolCv->notify_one();
 
   return Future<void>{ promiseFutureState };
 }
--- src/async/async.h
+++ src/async/async.h
@@ -32,18 +32,103 @@ int ExecuteProgram(std::function<int()>&&);
 // -----------------------------------------------------------------------------
 
 #include "promise_future_state.h"
+#include "promise.h"
+#include "thread_local_task_context.h"
 
+#include "telemetry/living_span.h"
+
+#include <condition_variable>
 #include <memory>
+#include <mutex>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+// -----------------------------------------------------------------------------
+
+namespace async::impl
+{
+
+// -----------------------------------------------------------------------------
+
+extern std::unordered_map<std::thread::id, std::thread> g_activeThreads;
+extern std::mutex g_activeThreadsMutex;
+extern std::vector<std::thread> g_completeThreads;
+extern std::mutex g_completeThreadsMutex;
+extern std::condition_variable_any g_allTasksCv;
+
+// -----------------------------------------------------------------------------
+
+} // async::impl
 
 // -----------------------------------------------------------------------------
 
 template <typename T>
-async::Future<T> async::RunTaskOnNewThread(std::function<T()>&& /*task*/)
+async::Future<T> async::RunTaskOnNewThread(std::function<T()>&& task)
 {
   std::shared_ptr<impl::PromiseFutureState<T>> promiseFutureState
     = std::make_shared<impl::PromiseFutureState<T>>();
 
-  // TODO: Implement
+  std::shared_ptr<bool> threadAddedToActivePool = std::make_shared<bool>(false);
+  std::shared_ptr<std::mutex> threadAddedToActivePoolMutex
+    = std::make_shared<std::mutex>();
+  std::shared_ptr<std::condition_variable> threadAddedToActivePoolCv
+    = std::make_shared<std::condition_variable>();
+
+  std::thread childThread{ [
+    task = std::move(task),
+    promise = impl::Promise{ promiseFutureState },
+    span = GetActiveSpan(),
+    threadAddedToActivePool,
+    threadAddedToActivePoolMutex,
+    threadAddedToActivePoolCv]() mutable
+    {
+      impl::ThreadLocalThreadTaskContext* context
+        = impl::CreateThreadLocalThreadTaskContext();
+      context->m_span = std::make_unique<telemetry::LivingSpan>(
+        span ?
+        telemetry::LivingSpan::Create(*span) :
+        telemetry::LivingSpan::Create());
+
+      T result = task();
+
+      context->m_span.reset();
+      impl::DestroyThreadLocalThreadTaskContext();
+
+      promise.Fulfill(std::move(result));
+
+      {
+        std::unique_lock lock{ *threadAddedToActivePoolMutex };
+        while (!*threadAddedToActivePool)
+        {
+          threadAddedToActivePoolCv->wait(lock);
+        }
+      }
+
+      {
+        std::scoped_lock lock{
+          impl::g_activeThreadsMutex,
+          impl::g_completeThreadsMutex };
+        std::thread completeThread
+          = std::move(impl::g_activeThreads.at(std::this_thread::get_id()));
+        impl::g_activeThreads.erase(std::this_thread::get_id());
+        impl::g_completeThreads.push_back(std::move(completeThread));
+      }
+      impl::g_allTasksCv.notify_one();
+    } };
+
+  {
+    std::lock_guard lock{ impl::g_activeThreadsMutex };
+    impl::g_activeThreads.emplace(childThread.get_id(), std::move(childThread));
+  }
+  impl::g_allTasksCv.notify_one();
+
+  {
+    std::lock_guard lock{ *threadAddedToActivePoolMutex };
+    *threadAddedToActivePool = true;
+  }
+  threadAddedToActivePoolCv->notify_one();
 
   return Future<T>{ promiseFutureState };
 }
