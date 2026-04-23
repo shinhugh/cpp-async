789a1230edca9bbc9365eeee2b56b859fd012e32
Hugh Shin <hughbee135@gmail.com>
2026-04-23 00:30:54 -0700

    Add stubs for RunTaskOnNew* functions

--- src/async/async.cpp
+++ src/async/async.cpp
@@ -1,6 +1,27 @@
 #include "async.h"
 
+#include "future.h"
+
 #include <functional>
+
+// -----------------------------------------------------------------------------
+
+template <>
+async::Future<void> async::RunTaskOnNewThread(std::function<void()>&& /*task*/)
+{
+  // TODO: Implement
+  return Future<void>{};
+}
+
+// -----------------------------------------------------------------------------
+
+template <>
+async::Future<void> async::RunTaskOnNewCoroutine(
+  std::function<void()>&& /*task*/)
+{
+  // TODO: Implement
+  return Future<void>{};
+}
 
 // -----------------------------------------------------------------------------
 
