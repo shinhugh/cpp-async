0105a15f0a3a468d36f10b9868ee650f799ab5a3
Hugh Shin <hughbee135@gmail.com>
2026-04-29 19:48:28 -0700

    Implement RunTaskOnNewThread functions

--- src/async/async.cpp
+++ src/async/async.cpp
@@ -2,12 +2,29 @@
 
 #include "future.h"
 #include "promise_future_state.h"
+#include "promise.h"
+#include "thread_local_task_context.h"
 
+#include "telemetry/living_span.h"
 #include "telemetry/span.h"
 
+#include <condition_variable>
+#include <cstddef>
 #include <functional>
 #include <memory>
+#include <mutex>
 #include <optional>
+#include <thread>
+#include <utility>
+#include <vector>
+
+// -----------------------------------------------------------------------------
+
+size_t async::impl::g_threadCount = 0;
+std::mutex async::impl::g_threadCountMutex;
+std::vector<std::thread> async::impl::g_completeThreads;
+std::mutex async::impl::g_completeThreadsMutex;
+std::condition_variable_any async::impl::g_allTasksCv;
 
 // -----------------------------------------------------------------------------
 
@@ -20,12 +37,44 @@ std::optional<telemetry::Span> async::GetActiveSpan()
 // -----------------------------------------------------------------------------
 
 template <>
-async::Future<void> async::RunTaskOnNewThread(std::function<void()>&& /*task*/)
+async::Future<void> async::RunTaskOnNewThread(std::function<void()>&& task)
 {
   std::shared_ptr<impl::PromiseFutureState<void>> promiseFutureState
     = std::make_shared<impl::PromiseFutureState<void>>();
 
-  // TODO: Implement
+  {
+    std::lock_guard lock{ impl::g_threadCountMutex };
+    impl::g_threadCount++;
+  }
+  impl::g_allTasksCv.notify_one();
+
+  std::shared_ptr<std::thread> childThread = std::make_shared<std::thread>();
+  *childThread = std::thread{ [
+    task = std::move(task),
+    promise = impl::Promise{ promiseFutureState },
+    span = GetActiveSpan(),
+    thisThread = childThread]() mutable
+    {
+      impl::ThreadLocalThreadTaskContext* context
+        = impl::CreateThreadLocalThreadTaskContext();
+      context->m_span = std::make_unique<telemetry::LivingSpan>(
+        span ?
+        telemetry::LivingSpan::Create(*span) :
+        telemetry::LivingSpan::Create());
+
+      task();
+
+      context->m_span.reset();
+      impl::DestroyThreadLocalThreadTaskContext();
+
+      promise.Fulfill();
+
+      {
+        std::lock_guard lock{ impl::g_completeThreadsMutex };
+        impl::g_completeThreads.push_back(std::move(*thisThread));
+      }
+      impl::g_allTasksCv.notify_one();
+    } };
 
   return Future<void>{ promiseFutureState };
 }
--- src/async/async.h
+++ src/async/async.h
@@ -32,18 +32,77 @@ int ExecuteProgram(std::function<int()>&&);
 // -----------------------------------------------------------------------------
 
 #include "promise_future_state.h"
+#include "promise.h"
+#include "thread_local_task_context.h"
 
+#include "telemetry/living_span.h"
+
+#include <condition_variable>
+#include <cstddef>
 #include <memory>
+#include <mutex>
+#include <thread>
+#include <utility>
+#include <vector>
+
+// -----------------------------------------------------------------------------
+
+namespace async::impl
+{
+
+// -----------------------------------------------------------------------------
+
+extern size_t g_threadCount;
+extern std::mutex g_threadCountMutex;
+extern std::vector<std::thread> g_completeThreads;
+extern std::mutex g_completeThreadsMutex;
+extern std::condition_variable_any g_allTasksCv;
+
+// -----------------------------------------------------------------------------
+
+} // async::impl
 
 // -----------------------------------------------------------------------------
 
 template <typename T>
-async::Future<T> async::RunTaskOnNewThread(std::function<T()>&& /*task*/)
+async::Future<T> async::RunTaskOnNewThread(std::function<T()>&& task)
 {
   std::shared_ptr<impl::PromiseFutureState<T>> promiseFutureState
     = std::make_shared<impl::PromiseFutureState<T>>();
 
-  // TODO: Implement
+  {
+    std::lock_guard lock{ impl::g_threadCountMutex };
+    impl::g_threadCount++;
+  }
+  impl::g_allTasksCv.notify_one();
+
+  std::shared_ptr<std::thread> childThread = std::make_shared<std::thread>();
+  *childThread = std::thread{ [
+    task = std::move(task),
+    promise = impl::Promise{ promiseFutureState },
+    span = GetActiveSpan(),
+    thisThread = childThread]() mutable
+    {
+      impl::ThreadLocalThreadTaskContext* context
+        = impl::CreateThreadLocalThreadTaskContext();
+      context->m_span = std::make_unique<telemetry::LivingSpan>(
+        span ?
+        telemetry::LivingSpan::Create(*span) :
+        telemetry::LivingSpan::Create());
+
+      T result = task();
+
+      context->m_span.reset();
+      impl::DestroyThreadLocalThreadTaskContext();
+
+      promise.Fulfill(std::move(result));
+
+      {
+        std::lock_guard lock{ impl::g_completeThreadsMutex };
+        impl::g_completeThreads.push_back(std::move(*thisThread));
+      }
+      impl::g_allTasksCv.notify_one();
+    } };
 
   return Future<T>{ promiseFutureState };
 }
