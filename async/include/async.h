271e13ef62137a97968cedd8794a613370629003
Hugh Shin <hughbee135@gmail.com>
2026-05-14 14:20:58 -0700

    Implement promise scope creation

--- async/include/async.h
+++ async/include/async.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "future.h"
+#include "promise.h"
+
 #include <functional>
 
 // -----------------------------------------------------------------------------
@@ -9,9 +12,37 @@ namespace async
 
 // -----------------------------------------------------------------------------
 
+template <typename T>
+Future<T> RunOnCurrentContext(
+    const std::function<void(Promise<T>)> &);
+
+template <>
+Future<void> RunOnCurrentContext(
+    const std::function<void(Promise<void>)> &);
+
 int RunApplication(
     std::function<int(int, char *[])> &&application, int argc, char *argv[]);
 
 // -----------------------------------------------------------------------------
 
 }  // namespace async
+
+// -----------------------------------------------------------------------------
+
+#include "promise_future_state.h"
+
+#include <memory>
+
+// -----------------------------------------------------------------------------
+
+template <typename T>
+async::Future<T> async::RunOnCurrentContext(
+    const std::function<void(Promise<T>)> &task)
+{
+  std::shared_ptr<impl::PromiseFutureState<T>> promiseFutureState =
+      std::make_shared<impl::PromiseFutureState<T>>();
+
+  task(Promise{promiseFutureState});
+
+  return Future{promiseFutureState};
+}
--- async/src/async.cpp
+++ async/src/async.cpp
@@ -1,7 +1,26 @@
 #include "async.h"
 
+#include "future.h"
+#include "promise.h"
+#include "promise_future_state.h"
+
 #include <functional>
+#include <memory>
 #include <utility>
+
+// -----------------------------------------------------------------------------
+
+template <>
+async::Future<void> async::RunOnCurrentContext(
+    const std::function<void(Promise<void>)> &task)
+{
+  std::shared_ptr<impl::PromiseFutureState<void>> promiseFutureState =
+      std::make_shared<impl::PromiseFutureState<void>>();
+
+  task(Promise{promiseFutureState});
+
+  return Future{promiseFutureState};
+}
 
 // -----------------------------------------------------------------------------
 
